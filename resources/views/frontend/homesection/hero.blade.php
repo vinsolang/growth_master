9a26b34e60dd0505a7882381452b2f81f4264969
vinsolang <vinsolang9@gmail.com>
2026-02-22 10:25:40 +0700

    update loerk ti 100dorng

--- resources/views/frontend/homesection/hero.blade.php
+++ resources/views/frontend/homesection/hero.blade.php
@@ -343,7 +343,7 @@
     <section class="max-w-7xl mx-auto px-4 py-16">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
 
-        <!-- Card 1 -->
+        <!-- Card 111 -->
         <div class="group">
             <div class="overflow-hidden rounded-lg">
                 <img
